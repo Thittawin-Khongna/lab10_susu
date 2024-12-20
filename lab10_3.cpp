14f8c3bc9fd60a1f7bb919ffb3eae921719297b9
Thittawin <ptkk00177@gmail.com>
2024-12-20 23:44:53 +0700

    lab10_3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,43 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+
+{
+
+    int count = 0;
+
+    double sum = 0;
+
+    double sum_of_square = 0;
+
+    string textline;
+
+    ifstream source("score.txt");
+
+    while (getline(source, textline))
+    {
+
+        sum += stof(textline);
+
+        sum_of_square += pow(stof(textline),2);
+
+        count++;
+    }
+    double x = sum/count;
+
+    cout << "Number of data = " << count << "\n";
+
+    cout << setprecision(3);
+
+    cout << "Mean = " << x << "\n";
+
+    cout << "Standard deviation = " << pow((sum_of_square/count)-pow(x,2),0.5);
+}
